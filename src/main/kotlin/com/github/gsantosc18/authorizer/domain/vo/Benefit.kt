e282c2b0f6e81c522d03d16831672b8ba9cc74cd
Gedalias dos Santos Caldas <gsantos.c18@gmail.com>
2024-11-06 18:10:53 -0300

    fix: Fix validation if balance is enough in benefit and wallet

--- src/main/kotlin/com/github/gsantosc18/authorizer/domain/vo/Benefit.kt
+++ src/main/kotlin/com/github/gsantosc18/authorizer/domain/vo/Benefit.kt
@@ -2,11 +2,12 @@ package com.github.gsantosc18.authorizer.domain.vo
 
 import com.github.gsantosc18.others.benefit.enums.BenefitType
 import java.math.BigDecimal
+import java.math.BigDecimal.ZERO
 
 data class Benefit(
     val accountId: String,
     val balance: BigDecimal,
     val type: BenefitType
 ) {
-    fun isEnough(value: BigDecimal): Boolean = balance >= value
+    fun isEnough(value: BigDecimal): Boolean = balance >= value && (balance - value) > ZERO
 }
--- src/main/kotlin/com/github/gsantosc18/authorizer/domain/vo/Wallet.kt
+++ src/main/kotlin/com/github/gsantosc18/authorizer/domain/vo/Wallet.kt
@@ -1,10 +1,11 @@
 package com.github.gsantosc18.authorizer.domain.vo
 
 import java.math.BigDecimal
+import java.math.BigDecimal.ZERO
 
 data class Wallet(
     val accountId: String,
     val balance: BigDecimal
 ) {
-    fun isEnough(value: BigDecimal): Boolean = balance >= value
+    fun isEnough(value: BigDecimal): Boolean = balance >= value && (balance - value) > ZERO
 }
